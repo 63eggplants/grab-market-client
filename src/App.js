82c1a1206c1ad399d964920d312e3eb014c50ef2
63eggplants <63eggplants@gmail.com>
2021-09-09 19:44:31 +0900

    antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
+import 'antd/dist/antd.css';
 import './App.css';
 import MainPageComponent from './main/index';
 import ProductPage from './product/index';
 import UploadPage from './upload/index';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link, useHistory } from 'react-router-dom';
+import { Button } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
 
 function App() {
+  const history = useHistory();
+  console.log(history);
   return (
     <div>
-      <Switch>
-        <Route exact={true} path={'/'}>
-          <MainPageComponent />
-        </Route>
-        <Route exact={true} path={'/products/:id'}>
-          <ProductPage />
-        </Route>
-        <Route exact={true} path={'/upload'}>
-          <UploadPage />
-        </Route>
-      </Switch>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo.png" alt="logo" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push('/upload');
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path={'/'}>
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path={'/products/:id'}>
+            <ProductPage />
+          </Route>
+          <Route exact={true} path={'/upload'}>
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- src/main/index.js
+++ src/main/index.js
@@ -19,50 +19,43 @@ function MainPage() {
         console.error('에러 발생', error);
       });
   }, []);
+  console.log(products);
 
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" alt="" />
-        </div>
+      <div id="banner">
+        <img src="/images/banners/banner1.png" alt="banner" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" alt="banner" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
-                <Link className="product-link" to={`/products/${index}`}>
-                  <div>
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/products/${product.id}`}>
+                <div>
+                  <img
+                    className="product-img"
+                    src={product.imageUrl}
+                    alt="product"
+                  />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
                     <img
-                      className="product-img"
-                      src={product.imageUrl}
-                      alt="product"
-                    />
+                      className="product-avatar"
+                      src="/images/icons/avatar.png"
+                      alt="seller avatar"
+                    ></img>
+                    <span>{product.seller}</span>
                   </div>
-                  <div className="product-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
-                    <div className="product-seller">
-                      <img
-                        className="product-avatar"
-                        src="images/icons/avatar.png"
-                        alt="seller avatar"
-                      ></img>
-                      <span>{product.seller}</span>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
